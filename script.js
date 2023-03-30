4b77be1983880858b97461be2228926ad7ee26c3
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-03-29 22:23:27 -0700

    changed object array to var

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 //created object list of password creds to hold arrays of each category
-var passlist = {
+function generatePassword() {
 
-  passUpper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  passlower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  passnumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  passcharacter: ["!", "@" , "#", "$", "%", "^", "&", "*", "(", ")", "?", "~", "|", "/", "+"]
+var length = [];
+var passUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var passlower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var passnumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var passcharacter = ["!", "@" , "#", "$", "%", "^", "&", "*", "(", ")", "?", "~", "|", "/", "+"];
 
+
+passlength = prompt ("how long would you like your password? Please choose 8-20 characters.")
+if (passlength < 8 || passlength > 20) {
+  return "Valid entry needed.";
+} else {
+   alert("Your goldenpass will be" + passlength + "long." );
+  }
 }
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -18,8 +30,12 @@ function writePassword() {
 
   passwordText.value = password;
 
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+ 
