c50cae6c6d0023c61201752103c25ef70c71caf1
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-03-30 20:27:48 -0700

    added comments to JS

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var generateBtn = document.querySelector("#generate");
 //created object list of password creds to hold arrays of each category
 function generatePassword() {
 
+//variables to collect input 
 var passUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var passlower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var passnumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -11,7 +12,7 @@ var passcharacters = ["!", "@" , "#", "$", "%", "^", "&", "*", "(", ")", "?", "~
 var length = [];
 
 
-
+//prompt asking user for pass length and validate their responses
 passlength = prompt ("how long would you like your password? Please choose 8-20 characters.")
 if (passlength < 8 || passlength > 20) {
   return "Valid entry needed.";
@@ -47,7 +48,7 @@ if (passlength < 8 || passlength > 20) {
     alert("password WON'T include special characters.");
   }
 
-
+//boolean to have user enter a valid reponse if they respond to no to all var
 if (uppercase === false && lowercase === false) {
   return "Valid response needed.";
 }
@@ -56,33 +57,45 @@ if (numbers === false && characters === false) {
   return "Valid response needed.";
 }
 
+//used push to continue to add elements to array
+if (uppercase) {
+length.push(passUpper);
+}
+if (lowercase) {
+length.push(passlower);
+}
+if (numbers) {
+length.push(passnumbers);
+}
+if (characters) {
+length.push(passcharacters);
+}
 
-
+//set marker to 0 to have loop continue without repeating
+let marker = 0
 
 let goldenpass = ""
 for (let i = 0; i < passlength; i++) {
-  let lng = [math.floor(math.random() * passlength.length)];
-  goldenpass = goldenpass + passlength[lng];
+  const current = length[marker]
+  let rng = [Math.floor(Math.random() * current.length)];
+  goldenpass = goldenpass + current[rng];
+  marker += 1
+  if (marker >= length.length) {
+    marker = 0
+  }
 }
 return goldenpass;
+
 };
+
+
  
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
